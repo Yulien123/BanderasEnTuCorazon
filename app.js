b5223133f3d72a98fea9056278ade896a268b4ea
Julio Flores <juliofloresth@gmail.com>
2025-04-21 20:01:33 -0300

    Commit 2
    Sirviendo archivos estáticos desde la carpeta public

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
+//Variable de entorno para el puerto, Si no existe la variable de entorno PORT, se asigna el valor 3000
 const PORT = process.env.PORT ?? 3000
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Endpoint Raíz
 app.get('/', (req, res) => {
-    res.send('<h1>Hola Mundo</h1>')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 // Levanta el servidor en el puerto enviado por PORT
